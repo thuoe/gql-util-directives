b065be9ee75701b3445f528192acf054f475fc6f
Eddie Thuo <thuoeddie17@gmail.com>
2024-04-08 17:38:36 +0100

    test: create suite template

---
+++ test/log.test.ts
@@ -0,0 +1,5 @@
+describe('@log directive', () => {
+  it.todo('can write to a console, with log level, label, timestamp & message')
+  it.todo('can write to a log file with the preferred file name with a log level, label, timestamp & message')
+  it.todo('will throw error if the log level is not recognzied')
+})
